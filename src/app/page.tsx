035a96b9f132ba0fd33bf07245155387427c1635
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-19 15:03:03 +0530

    Added a new section
    New black section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Herobanner from "@/components/custom/herobaner/herobanner";
-
+import Slider from "@/components/custom/slider/slider";
 export default function Home() {
   return (
     <div className="">
-      <div className="flex align-center justify-center">
-        <Herobanner />
-      </div>
+      <Herobanner />
+      <Slider />
     </div>
   );
 }
---
+++ src/components/custom/slider/slider.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const slider = () => {
+  return (
+    <div className="bg-black rounded-md max-w-[880px] p-12 mx-auto text-4xl font-medium border text-center mb-20 ">
+      <p className="bg-gradient-to-r from-red-500 to-purple-500 bg-clip-text text-transparent">
+        The quick brown fox jumps over the lazy dog
+      </p>
+    </div>
+  );
+};
+
+export default slider;
