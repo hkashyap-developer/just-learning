939d23d8a487a1d1ef1645b37efc1c585542d61d
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-19 15:04:54 +0530

    test2

--- src/components/custom/slider/slider.tsx
+++ src/components/custom/slider/slider.tsx
@@ -4,7 +4,7 @@ const slider = () => {
   return (
     <div className="bg-black rounded-md max-w-[880px] p-12 mx-auto text-4xl font-medium border text-center mb-20 ">
       <p className="bg-gradient-to-r from-red-500 to-purple-500 bg-clip-text text-transparent">
-        The quick brown fox jumps over the lazy dog test
+        The quick brown fox jumps over the lazy dog test test2
       </p>
     </div>
   );
