ee3bd534d500f53ba8f26c3548c6f62b5b138290
sakshiprajapati-ui <prajapatisakshi026@gmail.com>
2025-08-20 10:55:15 +0530

    new components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import Image from "next/image";
+
 import Herobanner from "@/components/custom/herobaner/herobanner";
 import Slider from "@/components/custom/slider/slider";
+import Card from "@/components/custom/card/card";
+import Avatar from "@/components/custom/avatar/avatar";
+import Carousel from "@/components/custom/carousel/carousel";
+import Hovercard from "@/components/custom/hovercard/hovercard";
+import Drawer from "@/components/custom/drawer/drawer";
+import Button from "@/components/custom/button/button";
+import Radiogroup from "@/components/custom/radiogroup/radiogroup";
+
+
 export default function Home() {
   return (
-    <div className="">
-      <Herobanner />
+    <div>
+      <Herobanner/>
+      <Hovercard/>
+    
+      <Radiogroup/>
       <Slider />
+      <Avatar/>
+      <Card/>
+      <Carousel/>
+      <Drawer/>
+      <Button/>
+
     </div>
   );
 }
